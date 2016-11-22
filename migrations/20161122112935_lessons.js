2a3d9727f0341e2a390c46c009d07d0a44926faa
Stringer86 <stringer.stephen@gmail.com>
2016-11-22 11:39:50 -0800

    migration and seed files

---
+++ migrations/20161122112935_lessons.js
@@ -0,0 +1,26 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('lessons', (table) => {
+    table.increments();
+
+    table.integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE');
+
+    table.string('title').notNullable().defaultTo('');
+    table.string('category');
+    table.string('concept');
+    table.text('description').notNullable().defaultTo('');
+    table.boolean('published').notNullable();
+    table.text('body').notNullable().defaultTo('');
+    table.integer('likes');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('lessons');
+};
