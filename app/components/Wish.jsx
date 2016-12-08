d3725c6825945f68b92215187dd70f42f0f3a440
Stringer86 <stringer.stephen@gmail.com>
2016-12-08 11:59:25 -0800

    upvote and downvote working correctly

--- app/components/Wish.jsx
+++ app/components/Wish.jsx
@@ -3,6 +3,15 @@ import { Link } from 'react-router';
 import axios from 'axios';
 
 export default class Wish extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      upvoteActive: false,
+      downvoteActive: false
+    }
+  }
+
   upvoted() {
     const upvotes = this.props.data.upvotes + 1;
     axios.patch(`/api/wishList/${this.props.data.id}`, {
@@ -13,6 +22,8 @@ export default class Wish extends React.Component {
     })
     const wish = this.props.data;
     this.props.upVote(wish);
+    this.setState({ upvoteActive: true })
+    this.setState({ downvoteActive: false })
   }
 
   downVoted() {
@@ -25,6 +36,34 @@ export default class Wish extends React.Component {
     })
     const wish = this.props.data;
     this.props.downVote(wish);
+    this.setState({ downvoteActive: true })
+    this.setState({ upvoteActive: false} )
+  }
+
+  cancelUpvote() {
+    const upvotes = this.props.data.upvotes - 1;
+    axios.patch(`/api/wishList/${this.props.data.id}`, {
+      upvotes: upvotes
+    })
+    .catch((err) => {
+      return err;
+    })
+    const wish = this.props.data;
+    this.props.downVote(wish);
+    this.setState({ upvoteActive: false })
+  }
+
+  cancelDownvote() {
+    const upvotes = this.props.data.upvotes + 1;
+    axios.patch(`/api/wishList/${this.props.data.id}`, {
+      upvotes: upvotes
+    })
+    .catch((err) => {
+      return err;
+    })
+    const wish = this.props.data;
+    this.props.upVote(wish);
+    this.setState({ downvoteActive: false })
   }
 
   componentDidMount() {
@@ -37,9 +76,11 @@ export default class Wish extends React.Component {
         <div className="col s12 wish-item">
           <div className="row info">
             <div className="col s4">
-              <div className="arrow-up" onClick={this.upvoted.bind(this)}></div>
+            {!this.state.upvoteActive &&  <div className="arrow-up" onClick={this.upvoted.bind(this)}></div>}
+            {this.state.upvoteActive &&  <div className="arrow-up-active" onClick={this.cancelUpvote.bind(this)}></div>}
                 <p className="upvote-count">{this.props.data.upvotes}</p>
-              <div className="arrow-down" onClick={this.downVoted.bind(this)}></div>
+            {!this.state.downvoteActive && <div className="arrow-down" onClick={this.downVoted.bind(this)}></div>}
+            {this.state.downvoteActive && <div className="arrow-down-active" onClick={this.cancelDownvote.bind(this)}></div>}
             </div>
             <div className="col s7">
               <h4 className="wish-question"><strong>Question:</strong> {this.props.data.question}</h4>
