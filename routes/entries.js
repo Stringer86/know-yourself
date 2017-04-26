dd4ae6b88628efd9adfe94c3ada35c9d02fa05a5
Stringer86 <stringer.stephen@gmail.com>
2017-04-26 15:13:21 -0700

    bug fix

--- routes/entries.js
+++ routes/entries.js
@@ -11,8 +11,8 @@ const { camelizeKeys, decamelizeKeys } = require('humps');
 const watson = require('watson-developer-cloud');
 
 const TONE_ANALYZER = watson.tone_analyzer({
-  username: '65e1093d-c41f-41c1-9379-598eb04c9ea6', //change for heroku
-  password: 'apMipWUx86MJ',
+  username: process.env.WATSON_USERNAME, //change for heroku
+  password: process.env.WATSON_PW,
   version: 'v3',
   version_date: '2016-05-19'
 });
